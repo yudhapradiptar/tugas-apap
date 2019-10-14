df398fdee2963740eb89c508c2adcba46bb020c8
Yudha Pradipta Ramadan <yudha512000@gmail.com>
2019-10-14 11:21:01 +0700

    feat tugas: initialize springboot project

---
+++ sibat/src/test/java/apap/tugas/sibat/SibatApplicationTests.java
@@ -0,0 +1,16 @@
+package apap.tugas.sibat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SibatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
